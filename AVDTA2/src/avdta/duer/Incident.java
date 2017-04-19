e2ee981342af7b302189059c5f52356a9c4bdeb4
hdx <hdx@hdx-PC>
2017-04-18 23:16:10 -0500

    Incident class

---
+++ AVDTA2/src/avdta/duer/Incident.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avdta.duer;
+
+/**
+ *
+ * @author hdx
+ */
+public class Incident 
+{
+    private int id;
+    
+    public int getId()
+    {
+        return id;
+    }
+    
+        public int hashCode()
+    {
+        return id;
+    }
+}
