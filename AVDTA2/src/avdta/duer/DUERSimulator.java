9988ae2edcbcaac04e82c421bd1d98afe6eca106
ml26893 <ml26893@caee-gs-boyles5.austin.utexas.edu>
2017-04-27 14:29:43 -0500

    Added DUERSimulator

---
+++ AVDTA2/src/avdta/duer/DUERSimulator.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avdta.duer;
+
+import avdta.dta.DTASimulator;
+import avdta.network.node.Node;
+import avdta.project.DTAProject;
+import avdta.vehicle.route.Hyperpath;
+
+/**
+ *
+ * @author ml26893
+ */
+public class DUERSimulator extends DTASimulator
+{
+    /**
+     * Constructs this {@link DTASimulator} empty with the given project.
+     * @param project the project
+     */
+    public DUERSimulator(DTAProject project)
+    {
+        super(project);
+    }
+    
+    
+    
+    public Hyperpath osp(Node dest)
+    {
+        Hyperpath output = new Hyperpath();
+        
+        
+        
+        
+        
+        return output;
+    }
+}
