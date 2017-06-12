eb04d78d29f674a5b4f6a184691433001f961446
ml26893 <ml26893@caee-gs-boyles5.austin.utexas.edu>
2017-06-12 12:00:48 -0500

    VI implemented

--- AVDTA2/src/avdta/duer/DUERSimulator.java
+++ AVDTA2/src/avdta/duer/DUERSimulator.java
@@ -12,6 +12,7 @@ import static avdta.network.Simulator.rand;
 import avdta.network.link.CentroidConnector;
 import avdta.network.link.Link;
 import avdta.network.node.Node;
+import avdta.network.node.Zone;
 import avdta.project.DTAProject;
 import avdta.project.DUERProject;
 import avdta.vehicle.DriverType;
@@ -40,7 +41,7 @@ public class DUERSimulator extends DTASimulator
     private Incident activeIncident;
     private Set<IncidentEffect> null_effects;
     
-    private List<State> allStates;
+    private Map<Link, List<State>> allStates;
     
     /**
      * Constructs this {@link DTASimulator} empty with the given project.
@@ -70,13 +71,23 @@ public class DUERSimulator extends DTASimulator
     {
         super.setLinks(links);
         
-        allStates = new ArrayList<State>();
+        allStates = new HashMap<>();
         
         for(Link l : links)
         {
+            Set<Link> U = null;
+            
+            List<State> temp = new ArrayList<>();
+            allStates.put(l, temp);
             for(Incident i : incidents)
             {
-                allStates.add(new State(l, i));
+                State state = new State(l, i);
+                if(U == null)
+                {
+                    U = state.getActionSpace(DriverType.AV);
+                }
+                state.U = U;
+                temp.add(state);
             }
         }
     }
@@ -298,16 +309,163 @@ public class DUERSimulator extends DTASimulator
 
     }
     
+    public static final double error_bound = 1.0e-4;
     
+    // for simplicity, ignore driver type
     public Hyperpath osp(Node dest, DriverType driver)
     {
+        // initialize
+        for(Link l: allStates.keySet())
+        {
+            for(State s : allStates.get(l))
+            {
+                s.J = Integer.MAX_VALUE;
+                s.mu = null;
+            }
+        }
+        
+        double error = 0.0;
+        
+        int iter = 0;
+        do
+        {
+            iter++;
+            error = vi_iter(dest);
+            
+            System.out.println(iter+"\t"+error);
+        }
+        while(error < error_bound);
+        
+        // calculate best policy
+        vi_iter(dest);
+        
         Hyperpath output = new Hyperpath();
         
+        for(Link l : allStates.keySet())
+        {
+            for(State s : allStates.get(l))
+            {
+                output.setNextLink(s.getLink(), s.getIncident(), s.mu);
+            }
+        }
         
-        
-        
-        
+        // origins
+        for(Node n : getNodes())
+        {
+            if(n.isZone() && n.getOutgoing().size() > 0)
+            {
+                Link min = null;
+                double best = Integer.MAX_VALUE;
+                
+                for(Link l : n.getOutgoing())
+                {
+                    State nullI = null;
+                    // find null incident state
+                    for(State s : allStates.get(l))
+                    {
+                        if(s.getIncident() == Incident.NULL)
+                        {
+                            nullI = s;
+                            break;
+                        }
+                    }
+                    
+                    if(nullI.J < best)
+                    {
+                        best = nullI.J;
+                        min = l;
+                    }
+                }
+                // store in hyperpath
+                output.setNextLink(n, best, min);
+            }
+        }
         
         return output;
+    }
+    
+    private double vi_iter(Node dest)
+    {
+        double error = 0.0;
+        for(Link link : allStates.keySet())
+        {
+            for(State s : allStates.get(link))
+            {
+                Incident inc = s.getIncident();
+
+                double newJ = Integer.MAX_VALUE;
+                double g = avgTT.get(inc).get(link);
+
+                if(link.getDest() == dest)
+                {
+                    newJ = avgTT.get(inc).get(link);
+                    s.mu = null;
+                }
+                else
+                {
+                    Link bestMu = null;
+
+                    // look through outgoing links
+                    for(Link l : s.U)
+                    {
+                        double expJ = 0.0;
+
+                        // calculate transition
+                        if(inc != Incident.NULL)
+                        {
+                            expJ = avgTT.get(inc).get(l);
+                        }
+                        else
+                        {
+                            State nullIncident = null;
+                            double totalProb = 0.0;
+
+                            for(State sp : allStates.get(link))
+                            {
+                                Incident ip = sp.getIncident();
+
+                                if(ip == Incident.NULL)
+                                {
+                                    nullIncident = sp;
+                                    continue;
+                                }
+
+                                double prob;
+
+                                if(isObservable(l, ip))
+                                {
+                                    prob = ip.getProbabilityOn();
+                                }
+                                else
+                                {
+                                    prob = l.getDest().getVMS().getProbOfInformation(ip) * ip.getProbabilityOn();
+                                }
+
+                                totalProb += prob;
+
+                                expJ += sp.J * prob;
+                            }
+
+                            // calculate null incident probability separately
+                            expJ += nullIncident.J * (1 - totalProb);
+                        }
+
+                        double temp = g + expJ;
+
+                        if(temp < newJ)
+                        {
+                            newJ = temp;
+                            bestMu = l;
+                        }
+                    }
+
+                    s.mu = bestMu;
+                }
+
+                error = Math.max(error, Math.abs(newJ - s.J));
+                s.J = newJ;
+            }
+        }
+        return error;
     }
 }
